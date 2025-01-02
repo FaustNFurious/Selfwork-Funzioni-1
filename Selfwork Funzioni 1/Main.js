7edc891fc837e5946c1837c72aabc559cf85ca88
FaustNFurious <lorelego2003@gmail.com>
2025-01-02 21:17:32 +0100

    Add files via upload
    corretto esercizio lancio dei dadi

--- Selfwork Funzioni 1/Main.js
+++ Selfwork Funzioni 1/Main.js
@@ -1,18 +1,38 @@
-// Dichiarazione Variabili
-let dicePlayer1 = parseInt(prompt("Giocatore 1: Indicare quante volte tirare il dado"));
-let dicePlayer2 = parseInt(prompt("Giocatore 2: Indicare quante volte tirare il dado"));
-let totDice = 0;
-
-// Istruzioni per formulare la frase = "Un Anello per domarli, un Anello per trovarli, un Anello per ghermirli e nel buio incatenarli.“
+// Funzione per la somma dei tiri dei dadi
 function sommaTiroDadi(number) {
+    // Dichiarazione Variabili
+    let totDice1 = 0;
+    let totDice2 = 0;
 
     for (let i = 1; i <= number; i++) {
-        totDice = totDice + Math.floor(Math.random()*(6-1) + 1);
-        console.log(`Al tiro n°${i} il punteggio è ${totDice}`);
+        // Dichiarazione lanci random
+        let rollRandom1 = Math.floor(Math.random() * (6-1) + 1);
+        let rollRandom2 = Math.floor(Math.random() * (6-1) + 1);
 
+        totDice1 = totDice1 + rollRandom1;
+        totDice2 = totDice2 + rollRandom2;
+
+        console.log(`Giocatore 1: Al tiro n°${i} il punteggio è ${totDice1}`);
+        console.log(`Giocatore 2: Al tiro n°${i} il punteggio è ${totDice2}`);
+
+    }
+
+    if (totDice1 > totDice2) {
+        return `Vince il Giocatore 1 totalizzando ${totDice1} punti`;
+    }
+
+    else if (totDice1 < totDice2) {
+        return `Vince il Giocatore 2 totalizzando ${totDice2} punti`;
+    }
+
+    else {
+        return `Entrambi i giocatori pareggiano totalizzando ${totDice1} punti`;
     }
 
 }
 
-let sommaPlayer1 = sommaTiroDadi(dicePlayer1);
-let sommaPlayer2 = sommaTiroDadi(dicePlayer2);
+// Numero di lanci dei dadi inserito dall'utente
+let dicePlayers = parseInt(prompt("Indicare quante volte tirare il dado"));
+
+let sommaPlayers = sommaTiroDadi(dicePlayers);
+console.log(sommaPlayers);
